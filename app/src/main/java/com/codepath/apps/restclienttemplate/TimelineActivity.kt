3d875477d3fc3c80bba17213f06f407bbb39f98d
gtoledorodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2022-03-11 12:33:14 -0600

    Fixed Refresh

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.kt
@@ -63,6 +63,7 @@ class TimelineActivity : AppCompatActivity() {
                     adapter.clear()
                     val listOfNewTweetsRetrieved = Tweet.fromJsonArray(jsonArray)
                     tweets.addAll(listOfNewTweetsRetrieved)
+                    adapter.addAll(tweets)
                     adapter.notifyDataSetChanged()
                     swipeContainer.setRefreshing(false)
                 } catch (e: Exception) {
@@ -77,6 +78,7 @@ class TimelineActivity : AppCompatActivity() {
                 throwable: Throwable?
             ) {
                 Log.i(TAG, "onFailure $statusCode")
+                Log.d("DEBUG", "Fetch timeline error", throwable)
             }
         })
     }
